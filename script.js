782550be16ab1d87a657ba2588c98d3651d1d40e
kdarwish00 <110360806+kdarwish00@users.noreply.github.com>
2022-12-26 22:48:45 +0000

    updated

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-/* JavaScript for adding functionality to the calculator */
 const input = document.getElementById("input");
-const buttons = document.querySelectorAll("#buttons button");
+const buttons = document.querySelectorAll("button");
 const clear = document.getElementById("clear");
 const equals = document.getElementById("equals");
 const decimal = document.getElementById("decimal");
@@ -11,35 +10,25 @@ const squareroot = document.getElementById("square-root")
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
-    // Get the button's value
     const value = this.innerText;
-    // If the value is a number, append it to the input field
     if (!isNaN(value)) {
       input.value += value;
     }
-    // If the value is an operator, append it to the input field
-    // unless the last character in the input field is already
-    // an operator, in which case do nothing
     else if (operators.indexOf(value) > -1) {
       if (operators.indexOf(input.value.slice(-1)) === -1) {
         input.value += value;
       }
     }
-    // If the value is the clear button, clear the input field
     else if (value === "CE") {
       input.value = "";
     }
-    // If the value is the equals button, perform the calculation
-    // and display the result
     else if (value === "=") {
       const equation = input.value;
       input.value = eval(equation);
     }
-    //If the value is the percentage button, divide value by 100
     else if(value === "%") {
       input.value /=  100
     }
-    //Toggles between positive and negative
     else if(value === "+/-" ) {
       if (input.value.startsWith("-")) {
         input.value = input.value.substring(1);
@@ -47,14 +36,10 @@ for (let i = 0; i < buttons.length; i++) {
         input.value = "-" + input.value;
       }
     }
-    //Finds the square root vaue of the input
 
     else if(value === "√") {
         input.value = Math.sqrt(input.value);
     }
-    // If the value is the decimal button, add a decimal point
-    // to the input field unless one already exists or the
-    // last character in the input field is an operator
     else if (value === ".") {
       if (input.value.indexOf(".") === -1 &&
           operators.indexOf(input.value.slice(-1)) === -1) {
